12c4a1d6b37f6ebfbeb0b0a3ceae31e181ddfc59
Darcy <DarcyMargetts@gmail.com>
2020-04-23 02:01:20 -0500

    created sign in form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomePage from "./components/homepage";
 import ShopPage from "./components/shop/shop";
 import Header from "./components/header/header";
+import SignInAndSignUp from "./components/signin-and-signup/signin-and-signup";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/components/signin-and-signup/signin-and-signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../sign-in/sign-in";
+import "./signin-and-signup.scss";
+
+const SigninAndSignUp = () => (
+  <div className="sign-in-and-sign-up">
+    SIGN IN
+    <SignIn />
+  </div>
+);
+
+export default SigninAndSignUp;
